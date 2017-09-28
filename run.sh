d4407ce48118f22c5a77ccdd4cc6a3d70eba1ee8
mwas <francismwangi152@gmail.com>
2017-09-28 06:22:30 +0300

    returned default port to 80

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --noinput
 # debugging with default server uncomment this and comment the gunicorn one
 #python manage.py runserver 0.0.0.0:3000 --settings=$DJANGO_SETTINGS_MODULE
 
-exec  gunicorn --bind=0.0.0.0:${PORT:-3000} UssdAirflowSeedProject.wsgi \
+exec  gunicorn --bind=0.0.0.0:${PORT:-80} UssdAirflowSeedProject.wsgi \
         --workers=5\
         --log-level=info \
         --log-file=-\
